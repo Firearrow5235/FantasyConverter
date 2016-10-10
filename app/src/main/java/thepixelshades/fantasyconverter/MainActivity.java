5e3c013eaf9b857142d30fe7c28515bd86d947a3
Gavyn Leavitt <firearrow5235@gmail.com>
2016-10-10 09:39:59 -0400

    Implemented settings and conversions from Gold/Silver/Copper.

--- app/src/main/java/thepixelshades/fantasyconverter/MainActivity.java
+++ app/src/main/java/thepixelshades/fantasyconverter/MainActivity.java
@@ -1,38 +1,95 @@
 package thepixelshades.fantasyconverter;
 
-import android.support.v7.app.ActionBarActivity;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //set spinner content
+
+        Spinner fantasy = (Spinner) findViewById(R.id.fantasy_spinner);
+        ArrayAdapter<CharSequence> fantasyAdapter = ArrayAdapter.createFromResource(this, R.array.mon_fantasy, android.R.layout.simple_spinner_item);
+        fantasyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fantasy.setAdapter(fantasyAdapter);
+
+        Spinner real = (Spinner) findViewById(R.id.real_spinner);
+        ArrayAdapter<CharSequence> realAdapter = ArrayAdapter.createFromResource(this, R.array.mon_real, android.R.layout.simple_spinner_item);
+        fantasyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        real.setAdapter(realAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void calculate(View view){
+        EditText fantValue = (EditText) findViewById(R.id.fantasy_value);
+        TextView realValue = (TextView) findViewById(R.id.real_value);
+        Spinner fantSpinner = (Spinner) findViewById(R.id.fantasy_spinner);
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        Resources res = getResources();
+
+        int coinRadius = Integer.parseInt(pref.getString("pref_coin_width", ""));
+        coinRadius = coinRadius / 2;
+        float coinThickness = Float.parseFloat(pref.getString("pref_coin_thickness", ""));
+
+        float coinVolumeMM = (float) ((coinRadius * coinRadius) * Math.PI * coinThickness);
+        float coinMass = (float) (coinVolumeMM / 10) * res.getFraction(R.dimen.gold_density, 1, 1) / 100;
+        float coinPrice = (float) coinMass * res.getFraction(R.dimen.gold_price, 1, 1);
+
+        switch (fantSpinner.getSelectedItem().toString()){
+            case "Gold":
+                break;
+            case "Silver":
+                coinPrice = coinPrice / 10;
+                break;
+            case "Copper":
+                coinPrice = coinPrice / 100;
+        }
+
+        realValue.setText(df.format(coinPrice * Integer.parseInt(fantValue.getText().toString())));
     }
 }
